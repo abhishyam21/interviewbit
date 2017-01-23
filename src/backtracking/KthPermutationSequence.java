0b07046a5646e76eb8ebdabe026662d90569aa68
abhishyam <chabhishyam@gmail.com>
2017-01-23 21:54:54 +0530

    added some problems from backtracking

---
+++ src/backtracking/KthPermutationSequence.java
@@ -0,0 +1,53 @@
+package backtracking;
+
+import java.util.*;
+
+/**
+ * Created by Rachana Rao on 1/23/2017.
+ */
+public class KthPermutationSequence {
+    private static List<String> result = new ArrayList<>();
+    public static void main(String[] args) {
+        KthPermutationSequence kthPermutationSequence = new KthPermutationSequence();
+        int n=3,k=4;
+        System.out.println(kthPermutationSequence.getPermutation(2,1));
+    }
+
+    private String getPermutation(int n, int k) {
+       ArrayList<Integer> str = new ArrayList<>();
+        for (int i = 1; i <=n ; i++) {
+            str.add(i);
+        }
+       permute(str,0,k);
+        String temp = result.get(k-1);
+        return temp;
+    }
+
+    private void permute(ArrayList<Integer> str, int pos, int k) {
+        if(pos == str.size()) {
+            if(result.size() > k)
+                return;
+            result.add(getString(str));
+
+        }else
+            for (int i = pos; i < str.size() ; i++) {
+                swap(str,i,pos);
+                permute(str,pos+1, k);
+                swap(str,pos,i);
+            }
+    }
+
+    private String getString(ArrayList<Integer> str) {
+        String res = "";
+        for (int i = 0; i < str.size(); i++) {
+            res+=str.get(i)+"";
+        }
+        return res;
+    }
+
+    private void swap(ArrayList<Integer> a, int position, int i) {
+        Integer integer = a.get(position);
+        a.set(position,a.get(i));
+        a.set(i,integer);
+    }
+}
