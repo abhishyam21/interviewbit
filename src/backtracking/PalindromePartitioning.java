8ef3d237604903f12c102f029a237618b2c7efc8
abhishyam <chabhishyam@gmail.com>
2017-01-26 09:11:59 +0530

    added some problem from backtracking

--- src/backtracking/PalindromePartitioning.java
+++ src/backtracking/PalindromePartitioning.java
@@ -1,20 +1,57 @@
 package backtracking;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by abhishyam.c on 12/15/2016.
  */
 public class PalindromePartitioning {
+    ArrayList<ArrayList<String>> result = new ArrayList<>();
     public static void main(String[] args) {
-        String a = "nitin";
+        String a = "aab";
         PalindromePartitioning palindromePartitioning = new PalindromePartitioning();
-        List<String> palindromStrings = palindromePartitioning.getPalindromStrings(a);
-        System.out.println(palindromStrings.toString());
+        ArrayList<ArrayList<String>> palindromeStrings = palindromePartitioning.getPalindromStrings(a);
+        for (ArrayList<String> palindromeString : palindromeStrings)
+            System.out.println(palindromeString.toString());
     }
 
-    private List<String> getPalindromStrings(String a) {
+    private ArrayList<ArrayList<String>> getPalindromStrings(String a) {
+        allPalindromes(a,new ArrayList<>(),0,a.length());
+        return result;
+    }
+
+    private void allPalindromes(String a, ArrayList<String> subset, int position, int n) {
+        if(position >= n){
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+        for (int i = position; i < n; i++) {
+            String subStr = a.substring(position,i+1);
+            if(isPalindrome(subStr)) {
+                subset.add(subStr);
+                allPalindromes(a, subset, i+1, n);
+                subset.remove(subset.size()-1);
+            }
+        }
+    }
+
+
+    private boolean isPalindrome(String palindrome) {
+        if(palindrome == null || palindrome.isEmpty()) return false;
+        int len = palindrome.length()-1;
+        for (int i = 0; i < (palindrome.length()/2); i++) {
+            if(palindrome.charAt(i) != palindrome.charAt(len-i))
+                return false;
+        }
+        return true;
+    }
+
+}
+
+
+
+/*
+  private List<String> getPalindromStrings(String a) {
         ArrayList<String> result = new ArrayList<>();
         for (int i = 0; i < a.length(); i++) {
             String substr = a.substring(i,a.length());
@@ -25,7 +62,7 @@ public class PalindromePartitioning {
                 }
             }
         }
-      /*  Collections.sort(subsetsList, new Comparator<String>() {
+      /*  Collections.sort(result, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 int c = Integer.compare(o1.length(),o2.length());
@@ -34,15 +71,6 @@ public class PalindromePartitioning {
                 return c;
             }
         });*/
-        return result;
-    }
-
-    private boolean isPalindrom(String palindrom) {
-        int len = palindrom.length()-1;
-        for (int i = 0; i < (palindrom.length()/2); i++) {
-            if(palindrom.charAt(i) != palindrom.charAt(len-i))
-                return false;
-        }
-        return true;
-    }
-}
+   /*     return result;
+                }
+ */
