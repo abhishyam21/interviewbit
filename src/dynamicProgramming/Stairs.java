53f719fc7283ae6158ed2cc15703e5e598737ef5
Abhishyam <chabhishyam@gmail.com>
2017-02-06 12:45:57 +0530

    added one more problem from dp

---
+++ src/dynamicProgramming/Stairs.java
@@ -0,0 +1,28 @@
+package test.com.dynamic;
+
+import java.util.Arrays;
+
+/**
+ * Created by abhishyam.c on 2/3/2017.
+ */
+public class Stairs {
+    public static void main(String[] args) {
+        Stairs stairs = new Stairs();
+        System.out.println(stairs.climbStairs(4));
+    }
+
+    private int climbStairs(int count) {
+        int[] numbers = new int[count+1];
+        Arrays.fill(numbers,-1);
+        return climbStairsUtil(count,numbers);
+    }
+    private int climbStairsUtil(int count, int[] numbers){
+        if(count<=1){
+            return 1;
+        }
+        if(numbers[count] != -1) return numbers[count];
+        numbers[count] = climbStairs(count-1)+climbStairs(count-2);
+        return numbers[count];
+    }
+
+}
