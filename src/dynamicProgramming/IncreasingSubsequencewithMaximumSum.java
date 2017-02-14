493968506f27731e00e086a84ed5cf747998acdf
abhishyam <chabhishyam@gmail.com>
2017-02-14 07:44:17 +0530

    added different solutions for some problems

---
+++ src/dynamicProgramming/IncreasingSubsequencewithMaximumSum.java
@@ -0,0 +1,7 @@
+package dynamicProgramming;
+
+/**
+ * Created by Rachana Rao on 2/14/2017.
+ */
+public class IncreasingSubsequencewithMaximumSum {
+}
--- src/dynamicProgramming/LongestIncreasingSubsequence.java
+++ src/dynamicProgramming/LongestIncreasingSubsequence.java
@@ -13,8 +13,15 @@ public class LongestIncreasingSubsequence {
         Integer[] temp = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
         List<Integer> a = new ArrayList<>(Arrays.asList(temp));
         System.out.println(longestIncreasingSubsequence.lis(a));
+        longestIncreasingSubsequence.printlis(a);
     }
 
+    /**
+     * This method just returns the max length of
+     * increasing sub sequence
+     * @param a ist of input values
+     * @return longest increasing sub sequence
+     */
     private int lis(List<Integer> a) {
         int max = 1;
         int n = a.size();
@@ -28,5 +35,26 @@ public class LongestIncreasingSubsequence {
             max = Math.max(max,cache[i]);
         }
         return max;
+    }
+
+    private void printlis(List<Integer> a){
+        int max = 1;
+        int n = a.size();
+        int[] cache = new int[n];
+        Arrays.fill(cache,1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if(a.get(i) > a.get(j) && cache[i] <(cache[j]+1))
+                    cache[i] = cache[j]+1;
+            }
+            max = Math.max(max,cache[i]);
+        }
+        for (int i = cache.length - 1; i >= 0; i--) {
+            if(cache[i] == max){
+                System.out.print(a.get(i)+" ");
+                max--;
+            }
+        }
+        System.out.println();
     }
 }
