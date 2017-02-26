8cc06cb0460eb4500879da944fa5a6b3528f7c93
abhishyam <chabhishyam@gmail.com>
2017-02-26 06:40:19 +0530

    fixed bug in DungeonPrincess class

--- src/dynamicProgramming/DungeonPrincess.java
+++ src/dynamicProgramming/DungeonPrincess.java
@@ -13,43 +13,10 @@ public class DungeonPrincess {
         System.out.println(dungeonPrincess.getMinHealth(a));
     }
 
-    private int getMinHealth(ArrayList<ArrayList<Integer>> a) {
-        int m = a.size();
-        int n = a.get(0).size();
-        int[][] cache = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                    if(i==0 && j==0){
-                        cache[i][j] = Math.max(1,1-a.get(i).get(j));
-                    }else if(i == 0){
-                            cache[i][j] = Math.max(1,cache[i][j-1] -a.get(i).get(j));
-                        }
-                    else if(j ==0 ){
-                        cache[i][j] = Math.max(1,cache[i-1][j]-a.get(i).get(j));
-                    }else {
-                        int up = cache[i-1][j]-a.get(i).get(j);
-                        int side = cache[i][j-1]-a.get(i).get(j);
-                            cache[i][j] = Math.max(1,Math.min(up,side));
-                    }
-            }
-        }
-        print(cache);
-        return cache[m-1][n-1]+1;
-    }
-
-    private void print(int[][] cache) {
-        for (int i = 0; i < cache.length; i++) {
-            for (int j = 0; j < cache[0].length; j++) {
-                System.out.print(cache[i][j]+"  ");
-            }
-            System.out.println();
-        }
-    }
-
     private static ArrayList<ArrayList<Integer>> getMatrix() {
-        Integer[] temp1 = {-2,-3,3};
-        Integer[] temp2 = {-5,-10,1};
-        Integer[] temp3 = {10,30,-5};
+        Integer[] temp1 = {-2, -3, 3};
+        Integer[] temp2 = {-5, -10, 1};
+        Integer[] temp3 = {10, 30, -5};
         ArrayList<Integer> one = new ArrayList<>(Arrays.asList(temp1));
         ArrayList<Integer> two = new ArrayList<>(Arrays.asList(temp2));
         ArrayList<Integer> three = new ArrayList<>(Arrays.asList(temp3));
@@ -58,5 +25,39 @@ public class DungeonPrincess {
         a.add(two);
         a.add(three);
         return a;
+    }
+
+    private int getMinHealth(ArrayList<ArrayList<Integer>> a) {
+        int m = a.size();
+        int n = a.get(0).size();
+        int[][] cache = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int num = a.get(i).get(j);
+                if (i == m - 1 && j == n - 1) {
+                    if (num > 0) {
+                        cache[i][j] = 1;
+                    } else
+                        cache[i][j] = 1 - num;
+                } else if (i == m - 1) {
+                    cache[i][j] = Math.max(1, cache[i][j + 1] - num);
+                } else if (j == n-1) {
+                    cache[i][j] = Math.max(1, cache[i + 1][j] - num);
+                } else {
+                    cache[i][j] = Math.max(1, Math.min(cache[i + 1][j], cache[i][j + 1]) - num);
+                }
+            }
+        }
+        print(cache);
+        return cache[0][0];
+    }
+
+    private void print(int[][] cache) {
+        for (int i = 0; i < cache.length; i++) {
+            for (int j = 0; j < cache[0].length; j++) {
+                System.out.print(cache[i][j] + "  ");
+            }
+            System.out.println();
+        }
     }
 }
