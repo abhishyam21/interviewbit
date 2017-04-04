37f6dde5407cabc7aa31c8d5ff7718e169df0211
abhishyam <chabhishyam@gmail.com>
2017-04-04 22:45:07 +0530

    added a new problem into dynamic programming

---
+++ src/dynamicProgramming/outsideproblems/PartitionProblem.java
@@ -0,0 +1,39 @@
+package dynamicProgramming.outsideproblems;
+
+import java.util.Arrays;
+
+/**
+ * Problem Link:
+ * https://www.techiedelight.com/partition-problem/
+ * Created by Rachana Rao on 4/4/2017.
+ */
+public class PartitionProblem {
+    private static boolean[][] cache;
+    public static void main(String[] args) {
+        int arr[] = { 7, 3, 1, 5, 4, 8 };
+        if(partition(arr))
+            System.out.println("Yes");
+        else System.out.println("No");
+    }
+
+    private static boolean partition(int[] arr) {
+        int sum = Arrays.stream(arr).sum();
+        int n = arr.length;
+        cache = new boolean[n+1][sum+1];
+        for (int i = 0; i <= n; i++) {
+            cache[i][0] = true;
+        }
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=sum ; j++) {
+                if(j < arr[i-1])
+                    cache[i][j] = cache[i-1][j];
+                else
+                    cache[i][j] = cache[i-1][j] || cache[i-1][j-arr[i-1]];
+            }
+        }
+        for (boolean[] booleans : cache) {
+            System.out.println(Arrays.toString(booleans));
+        }
+        return cache[n][sum];
+    }
+}
