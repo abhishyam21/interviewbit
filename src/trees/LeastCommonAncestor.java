6c95de8f6665e5c86ae3f1b092486e612a3a8072
abhishyam <chabhishyam@gmail.com>
2017-04-25 00:01:57 +0530

    changes the logic

--- src/trees/LeastCommonAncestor.java
+++ src/trees/LeastCommonAncestor.java
@@ -4,6 +4,7 @@ package trees;
  * Created by Rachana Rao on 12/29/2016.
  */
 public class LeastCommonAncestor {
+    private int lca;
     public static void main(String[] args) {
         LeastCommonAncestor leastCommonAncestor = new LeastCommonAncestor();
         int[] temp = {20, 10, 30, 5, 15, 25, 35, 4, 6, 14, 16, 24, 26, 34, 36};
@@ -14,13 +15,33 @@ public class LeastCommonAncestor {
     }
 
     private int lca(TreeNode a, int val1, int val2) {
-        if(a == null) return -1;
-        if(a.val == val1 || a.val == val2) return a.val;
-        int left = lca(a.left,val1,val2);
-        int right = lca(a.right,val1,val2);
+        if (isNodeExists(a, val1) && isNodeExists(a, val2)) {
+            lcaUtil(a, val1, val2);
+            return lca;
+        } else return -1;
+    }
 
-        if(left != -1 && right != -1) return a.val;
-        if(left ==-1 && right == -1) return -1;
-        return left!=-1? left:right;
+    private boolean lcaUtil(TreeNode a, int val1, int val2) {
+        if (a == null) return false;
+        //if one of the node is find, then assign that to
+        //lca
+        if (a.val == val1 || a.val == val2) {
+            lca = a.val;
+            return true;
+        }
+        boolean left = lcaUtil(a.left, val1, val2);
+        boolean right = lcaUtil(a.right, val1, val2);
+
+        if (left && right) lca = a.val;
+        return left || right;
+    }
+
+    /**
+     * Check if node exists in the given tree
+     */
+    private boolean isNodeExists(TreeNode a, int val) {
+        if (a == null) return false;
+        if (a.val == val) return true;
+        return isNodeExists(a.left, val) || isNodeExists(a.right, val);
     }
 }
